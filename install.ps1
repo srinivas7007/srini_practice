86d72740ee9ab20f12a3af30be485203b6a3aaf8
srinivas7007 <88900967+srinivas7007@users.noreply.github.com>
2023-09-23 12:47:49 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,7 +3,7 @@ Set=ExecutionPolicy Unrestricted -Force
 [Net.ServicePointManger]::SecurtiyProtocol = [Net.SecurtiyProtocolType]::Tls12
 
 #Download terraform and unzip to c:/terraform folder
-$Path = $env :TEMP;
+$Path = $env:TEMP;
 cd $Path;
 $File = "terraform_1.5.7_windows_amd64.zip";
 $Destination = "C:\terraform";
